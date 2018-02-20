c9896e303da0f48d34a74dfc6be27c33d3aadf8d
jpboliv <jpoliveira11@gmail.com>
2018-02-20 15:22:00 +0000

    added title to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220121523) do
+ActiveRecord::Schema.define(version: 20180220151418) do
 
   create_table "posts", force: :cascade do |t|
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "title"
   end
 
   create_table "users", force: :cascade do |t|
