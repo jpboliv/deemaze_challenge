492ab9fc012508dae099b8c08e59dedd50f146ac
jpboliv <jpoliveira11@gmail.com>
2018-02-20 12:14:44 +0000

    added users with devise and connection between users and posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,49 @@
+class PostsController < ApplicationController
+    before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
+    before_action :authenticate_user!, except: [:index, :show]
+
+    def index
+        @posts=Post.all.order("created_at DESC")
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def new
+        @post = current_user.post.build
+    end
+    
+    def create
+        @post = current_user.post.build(post_params)
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    def edit 
+    end
+
+    def update 
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @post.destroy
+        redirect_to root_path
+    end
+
+    private 
+    def find_post
+    end
+
+    def post_params
+        params.require(:post).permit(:description)
+    end
+end
